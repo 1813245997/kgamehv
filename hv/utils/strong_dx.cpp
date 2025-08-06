3913292a8ead29d1a2ab388d8611b56645317d87
killer <85392775+1813245997@users.noreply.github.com>
2025-08-06 17:33:38 +0800

    FIX

--- hv/utils/strong_dx.cpp
+++ hv/utils/strong_dx.cpp
@@ -336,10 +336,10 @@ namespace utils
 		void draw_overlay_elements(int width, int height, void* data)
 		{
 
-				if (utils::auth::is_license_expired())
-				{
-					return;
-				}
+			if (utils::auth::is_license_expired())
+			{
+				return;
+			}
 			memset(g_pagehit, 0, sizeof(g_pagehit));
 			memset(g_pagevaild, 0, sizeof(g_pagevaild));
 
@@ -355,7 +355,7 @@ namespace utils
 			rend.FillCircle(circle_center, color, radius);
 			 
 
-			 
+			//rend.String( g_Font, { 100, 200 }, L"https://github.com/cs1ime", PM_XRGB(255, 0, 0));
 			// === ªÊ÷∆ ESP œ‡πÿ ===
 			draw_players_esp(rend);
 			 
